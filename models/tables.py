5b97bb6babdb1ca7f661269ea3efd9d04506121c
cmehrabian <cameron.mehrabian@gmail.com>
2015-01-13 18:50:58 -0800

    Inital Commit.

---
+++ models/tables.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+db.define_table('bboard', 
+                Field('name'),
+                Field('phone'),
+                Field('email'),
+                Field('date_posted', 'datetime'),
+                Field('message', 'text'),
+                )
